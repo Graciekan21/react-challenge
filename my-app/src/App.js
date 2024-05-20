6f3debe0ffb7e9b5d874b5dc3590db8851c68b22
Gracie Nemusesu <graciekan21@gmail.com>
2024-05-20 18:22:56 +0000

    Commit all changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
-import Clock from "./components/Clock";
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
-  return <div className="App">
-      <Clock />
-    </div>;
+  return (
+    <div className={css.App}>
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
+      <Content />
+    </div>
+  );
 }
 
 export default App;
